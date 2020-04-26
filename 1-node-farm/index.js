c30ba0558f048cfcdd7e14d4c0332e5caad3e93b
Celal <cllakyz@hotmail.com>
2020-04-26 19:44:26 +0300

    overview page

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -29,6 +29,25 @@ console.log('Will read file!');*/
 
 ///////////////////////////////////////////////////////
 // SERVER
+const replaceTemplate = (temp, product) => {
+    let notOrganic = '';
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    if (!product.organic)
+        notOrganic = 'not-organic'
+    output = output.replace(/{%NOT_ORGANIC%}/g, notOrganic);
+    return output;
+};
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
@@ -37,13 +56,21 @@ const server = http.createServer((req, res) => {
     const pathName = req.url;
 
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the OVERVIEW');
+        // Overview page
+        res.writeHead(200, { 'Content-type': 'text/html' });
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+        res.end(output);
     } else if (pathName === '/product') {
+        // Product page
         res.end('This is the PRODUCT');
     } else if(pathName === '/api') {
+        // API
         res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(dataObj);
     } else {
+        // Not found
         res.writeHead(404, {
             'Content-type': 'text/html',
             'my-own-header': 'hello-world'

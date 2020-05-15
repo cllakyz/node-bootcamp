7b639bbb75d3f7afcbfe56b1c2f2a4f8edb8199c
Celal <cllakyz@hotmail.com>
2020-05-16 01:57:20 +0300

    middleware third party

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1- MIDDLEWARES
+app.use(morgan('dev'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -15,6 +20,8 @@ app.use((req, res, next) => {
 });
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+// 2- ROUTE HANDLERS
 
 const getAllTours = (req, res) => {
     console.log(req.requestTime);
@@ -96,6 +103,8 @@ app.post('/api/v1/tours', createTour);
 app.patch('/api/v1/tours/:id', updateTour);
 app.delete('/api/v1/tours/:id', deleteTour);*/
 
+// 3- ROUTES
+
 app.route('/api/v1/tours')
     .get(getAllTours)
     .post(createTour);
@@ -104,6 +113,8 @@ app.route('/api/v1/tours/:id')
     .get(getTour)
     .patch(updateTour)
     .delete(deleteTour);
+
+// 4- START SERVER
 
 const port = 3000;
 app.listen(port, () => {

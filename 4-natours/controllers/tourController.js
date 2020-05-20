1f6ef0fe3418f2e7c85c8314a8e6645c91dd833f
Celal <celal7174@gmail.com>
2020-05-21 00:33:23 +0300

    route multiple middleware

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -13,6 +13,16 @@ exports.checkID = (req, res, next , val) => {
     next();
 };
 
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(404).json({
+            status: 'Fail',
+            message: 'Missing name or price'
+        });
+    }
+    next();
+};
+
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
     res.status(200).json({
@@ -40,7 +50,7 @@ exports.createTour = (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id: newId }, req.body);
     tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         res.status(201).json({
             status: 'success',
             data: {

c3e387bd91742e899e310aa21a06b8e1fd598765
Celal <celal7174@gmail.com>
2020-05-23 02:11:17 +0300

    create tour a model

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,15 +1,5 @@
 const Tour = require('./../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-    if (!req.body.name || !req.body.price) {
-        return res.status(404).json({
-            status: 'Fail',
-            message: 'Missing name or price'
-        });
-    }
-    next();
-};
-
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
     res.status(200).json({
@@ -32,14 +22,26 @@ exports.getTour = (req, res) => {
         }
     });*/
 };
-exports.createTour = (req, res) => {
+exports.createTour = async (req, res) => {
 
-    res.status(201).json({
-        status: 'success',
-        data: {
-            //tour: newTour
-        }
-    });
+    try {
+        /*const newTour = new Tour({});
+    newTour.save();*/
+
+        const newTour = await Tour.create(req.body);
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: 'Invalid data sent!'
+        });
+    }
 };
 exports.updateTour = (req, res) => {
     res.status(200).json({

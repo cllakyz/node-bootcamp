ec6e7031d3de4239b5c498a67be1cab4c4bc052a
Celal <celal7174@gmail.com>
2020-05-22 19:29:13 +0300

    db connection

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,10 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env'});
-
 const app = require('./app');
 
-// console.log(app.get('env'));
-// console.log(process.env);
+dotenv.config({ path: './config.env'});
+
+const DB = process.env.DATABASE.replace('<USERNAME>', process.env.DATABASE_USERNAME).replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log('DB connection successful!');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

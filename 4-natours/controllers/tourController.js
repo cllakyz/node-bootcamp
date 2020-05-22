faa723621b8b993b94af6701a6a3733d73caaded
Celal <celal7174@gmail.com>
2020-05-23 02:34:04 +0300

    update tour by the model

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -18,6 +18,7 @@ exports.getAllTours = async (req, res) => {
         });
     }
 };
+
 exports.getTour = async (req, res) => {
     try {
         const tour = await Tour.findById(req.params.id);
@@ -35,6 +36,7 @@ exports.getTour = async (req, res) => {
         });
     }
 };
+
 exports.createTour = async (req, res) => {
 
     try {
@@ -56,14 +58,27 @@ exports.createTour = async (req, res) => {
         });
     }
 };
-exports.updateTour = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: "<Updated tour here...>"
-        }
-    });
+
+exports.updateTour = async (req, res) => {
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 };
+
 exports.deleteTour = (req, res) => {
     res.status(204).json({
         status: 'success',

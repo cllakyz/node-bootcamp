7b14fa7b3bae23168da0161be4119238fab88148
Celal <celal7174@gmail.com>
2020-05-23 16:06:27 +0300

    import dev data

---
+++ 4-natours/dev-data/data/import-dev-data.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env'});
+
+const DB = process.env.DATABASE.replace('<USERNAME>', process.env.DATABASE_USERNAME).replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('DB connection successful!');
+});
+
+// Read Json File
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+// Import data into db
+const importData = async () => {
+    try {
+        await Tour.create(tours);
+        console.log('Data successfully loaded!');
+    } catch (e) {
+        console.log(e);
+    }
+    process.exit();
+};
+
+// Delete All Data From DB
+const deleteData = async () => {
+    try {
+        await Tour.deleteMany();
+        console.log('Data successfully deleted!');
+    } catch (e) {
+        console.log(e);
+    }
+    process.exit();
+};
+
+if (process.argv[2] === '--import') {
+    importData();
+} else if (process.argv[2] === '--delete') {
+    deleteData();
+}
+
+console.log(process.argv);

e3fb2082973723dcd820fd8011c5103693ed4127
Celal <celal7174@gmail.com>
2020-05-30 13:29:50 +0300

    unhandled routes errors

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -12,21 +12,23 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.json());
 
-app.use((req, res, next) => {
-    console.log('Hello from the middleware.');
-    next();
-});
+// STATIC FOLDERS
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 });
 
-// STATIC FOLDERS
-app.use(express.static(`${__dirname}/public`))
-
 // 2- ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server!`
+    });
+});
 
 module.exports = app;

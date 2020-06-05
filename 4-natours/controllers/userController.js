bf09ecc3434b188ba06ae510e2f8dbf40b405540
Celal <celal7174@gmail.com>
2020-06-06 01:17:32 +0300

    login jwt sign

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -1,9 +1,28 @@
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
+const User = require('./../models/userModel');
+const APIFeatures = require('./../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+    // BUILD QUERY
+    const features = new APIFeatures(User.find(), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
+    // EXECUTE QUERY
+    const users = await features.query;
+
+    // SEND RESPONSE
+    res.status(200).json({
+        status: 'success',
+        requestedAt: req.requestTime,
+        results: users.length,
+        data: {
+            users
+        }
     });
-};
+});
 exports.createUser = (req, res) => {
     res.status(500).json({
         status: 'error',

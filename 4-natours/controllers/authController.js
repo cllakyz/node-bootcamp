451977a5e4dbb17c9d31adbce3d47c0606f692f9
Celal <celal7174@gmail.com>
2020-06-06 14:33:26 +0300

    auth user roles and permission

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -16,7 +16,8 @@ exports.signup = catchAsync(async (req, res, next) => {
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-        passwordConfirm: req.body.passwordConfirm
+        passwordConfirm: req.body.passwordConfirm,
+        role: req.body.role
     });
 
     const token = signToken(newUser._id);
@@ -76,3 +77,12 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.user = currentUser;
     next();
 });
+
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(new AppError('You do not have permission to perform this action', 403));
+        }
+        next();
+    }
+};

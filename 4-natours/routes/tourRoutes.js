13082ce53e8e35afd5cdf06356f41f5c364a6d14
Celal <celal7174@gmail.com>
2020-06-06 03:20:12 +0300

    tour routes protected

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -7,21 +7,21 @@ const router = express.Router();
 // router.param('id', tourController.checkID);
 
 router.route('/top-5-cheap')
-    .get(tourController.aliasTopTours, tourController.getAllTours);
+    .get(authController.protect, tourController.aliasTopTours, tourController.getAllTours);
 
 router.route('/tour-stats')
-    .get(tourController.getTourStats);
+    .get(authController.protect, tourController.getTourStats);
 
 router.route('/monthly-plan/:year')
-    .get(tourController.getMonthlyPlan);
+    .get(authController.protect, tourController.getMonthlyPlan);
 
 router.route('/')
     .get(authController.protect, tourController.getAllTours)
     .post(authController.protect, tourController.createTour);
 
 router.route('/:id')
-    .get(tourController.getTour)
-    .patch(tourController.updateTour)
-    .delete(tourController.deleteTour);
+    .get(authController.protect, tourController.getTour)
+    .patch(authController.protect, tourController.updateTour)
+    .delete(authController.protect, tourController.deleteTour);
 
 module.exports = router;

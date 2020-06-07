c6d9c83d358e69bd535039c8d0e5e0edf536ce97
Celal <celal7174@gmail.com>
2020-06-08 01:52:00 +0300

    set security package helmet

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,6 +1,7 @@
 const express   = require('express');
 const morgan    = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet    = require('helmet');
 
 const AppError      = require('./utils/appError');
 const errorHandler  = require('./controllers/errorController');
@@ -11,10 +12,15 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+// Limit requests from same IP
 const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000,
@@ -22,17 +28,19 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
 
-// 2) STATIC FOLDERS
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 });
 
-// 3) ROUTES
+// 2) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 

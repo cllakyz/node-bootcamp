7094bd672ffadc20b870f12109683f145aaa96c9
Celal <celal7174@gmail.com>
2020-06-29 11:10:06 +0300

    error view

--- 4-natours/controllers/viewsController.js
+++ 4-natours/controllers/viewsController.js
@@ -1,7 +1,6 @@
 const Tour          = require('./../models/tourModel');
 const catchAsync    = require('./../utils/catchAsync');
 const AppError      = require('./../utils/appError');
-const factory       = require('./handlerFactory');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
     // 1) Get tour data from collection
@@ -22,6 +21,11 @@ exports.getTour = catchAsync(async (req, res, next) => {
         path: 'reviews',
         fields: 'review rating user'
     });
+
+    if (!tour) {
+        return next(new AppError('There is no tour with that name.', 404));
+    }
+
     // 2) Build template
 
     // 3) Render template using data from 1)

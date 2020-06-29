eceef32f632c13d618633a9936fe140e304da5c5
Celal <celal7174@gmail.com>
2020-06-29 10:33:19 +0300

    user logout

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -63,6 +63,14 @@ exports.login = catchAsync(async (req, res, next) => {
     createSendToken(user, 200, res);
 });
 
+exports.logout = (req, res) => {
+    res.cookie('jwt', 'loggedout', {
+        expires: new Date(Date.now() - 10 * 1000),
+        httpOnly: true
+    });
+    res.status(200).json({ status: 'success' });
+}
+
 exports.protect = catchAsync(async (req, res, next) => {
     let token;
     // 1) Getting token and check of it's there
@@ -103,26 +111,30 @@ exports.restrictTo = (...roles) => {
 };
 
 // Only for rendered pages, no errors!
-exports.isLoggedIn = catchAsync(async (req, res, next) => {
+exports.isLoggedIn = async (req, res, next) => {
     if(req.cookies.jwt) {
-        // 1) Verification token
-        const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET);
-        // 2) Check if user still exists
-        const currentUser = await User.findById(decoded.id);
-        if (!currentUser) {
-            return next();
-        }
-        // 3) Check if user changed password after the token was issued
-        if (currentUser.changedPasswordAfter(decoded.iat)) {
-            return next();
-        }
+        try {
+            // 1) Verification token
+            const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET);
+            // 2) Check if user still exists
+            const currentUser = await User.findById(decoded.id);
+            if (!currentUser) {
+                return next();
+            }
+            // 3) Check if user changed password after the token was issued
+            if (currentUser.changedPasswordAfter(decoded.iat)) {
+                return next();
+            }
 
-        // THERE IS A LOGGED IN USER
-        res.locals.user = currentUser;
-        return next();
+            // THERE IS A LOGGED IN USER
+            res.locals.user = currentUser;
+            return next();
+        } catch (e) {
+            return next();
+        }
     }
     next();
-});
+};
 
 exports.forgotPassword = catchAsync(async (req, res, next) => {
     // 1) Get user based on Posted email

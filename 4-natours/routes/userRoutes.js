191dc5741765976c6979411a6761da6754f7df63
Celal <celal7174@gmail.com>
2020-07-03 23:23:01 +0300

    upload file by the multer package

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -3,7 +3,6 @@ const express = require('express');
 const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
 
-
 const router = express.Router();
 
 router.post('/signup', authController.signup);
@@ -17,7 +16,7 @@ router.use(authController.protect);
 
 router.get('/me', userController.getMe, userController.getUser);
 router.patch('/updateMyPassword', authController.updatePassword);
-router.patch('/updateMe', userController.updateMe);
+router.patch('/updateMe', userController.uploadUserPhoto, userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
 
 router.use(authController.restrictTo('admin'));

4dc31990cfaa1bda8dbdb17f9cf523553e340b45
Celal <celal7174@gmail.com>
2020-07-03 22:13:47 +0300

    update user password with api

--- 4-natours/public/js/index.js
+++ 4-natours/public/js/index.js
@@ -1,13 +1,14 @@
 import '@babel/polyfill';
 import { displayMap } from "./mapbox";
 import { login, logout } from "./login";
-import { updateData } from "./updateSettings";
+import { updateSettings } from "./updateSettings";
 
 // DOM ELEMENTS
 const mapBox        = document.getElementById('map');
 const loginForm     = document.querySelector('.form--login');
 const logOutBtn     = document.querySelector('.nav__el--logout');
 const userDataForm  = document.querySelector('.form-user-data');
+const userPassForm  = document.querySelector('.form-user-password');
 
 // DELEGATION
 if (mapBox) {
@@ -16,11 +17,11 @@ if (mapBox) {
 }
 
 if (loginForm) {
-    loginForm.addEventListener('submit', e => {
+    loginForm.addEventListener('submit', async e => {
         e.preventDefault();
         const email     = document.getElementById('email').value;
         const password  = document.getElementById('password').value;
-        login(email, password);
+        await login(email, password);
     });
 }
 
@@ -29,10 +30,27 @@ if (logOutBtn) {
 }
 
 if (userDataForm) {
-    userDataForm.addEventListener('submit', e => {
+    userDataForm.addEventListener('submit', async e => {
         e.preventDefault();
         const name  = document.getElementById('name').value;
         const email = document.getElementById('email').value;
-        updateData(name, email);
+        await updateSettings({ name, email }, 'data');
+    });
+}
+
+if (userPassForm) {
+    userPassForm.addEventListener('submit', async e => {
+        e.preventDefault();
+        document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+        const passwordCurrent  = document.getElementById('password-current').value;
+        const password         = document.getElementById('password').value;
+        const passwordConfirm  = document.getElementById('password-confirm').value;
+        await updateSettings({ passwordCurrent, password, passwordConfirm }, 'password');
+
+        document.querySelector('.btn--save-password').textContent = 'Save password';
+        document.getElementById('password-current').value        = '';
+        document.getElementById('password').value                = '';
+        document.getElementById('password-confirm').value        = '';
     });
 }
--- 4-natours/public/js/updateSettings.js
+++ 4-natours/public/js/updateSettings.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 import { showAlert } from "./alerts";
 
-export const updateData = async (name, email) => {
+// type is either 'password' pr 'data'
+export const updateSettings = async (data, type) => {
     try {
+        const url = type === 'password' ? 'updateMyPassword' : 'updateMe';
+
         const res = await axios({
             method: 'PATCH',
-            url: 'http://127.0.0.1:3000/api/v1/users/updateMe',
-            data: {
-                name,
-                email
-            }
+            url: `http://127.0.0.1:3000/api/v1/users/${url}`,
+            data
         });
 
         if (res.data.status === 'success') {
-            showAlert('success', 'Data updated successfully.');
+            showAlert('success', `${type.toUpperCase()} updated successfully.`);
         }
 
     } catch (e) {
